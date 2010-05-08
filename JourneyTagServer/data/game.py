572e4aa6148aa960194976002747edc84db1b0dd
Ben Ford <bentford@gmail.com>
2010-05-08 04:15:36 -0700

    fixed bugs in highscore webservice call

--- JourneyTagServer/data/game.py
+++ JourneyTagServer/data/game.py
@@ -3,11 +3,14 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 import jt.model
+import jt.modelhelper
 import jt.service.gamescore
+import logging
 
 class GetAccountsByHighScore(webapp.RequestHandler):
     def get(self):
-        accounts = jt.service.getAccountsByHighScore(200)
+        accounts = jt.service.gamescore.getAccountsByHighScore(200)
+        logging.info('loaded %d accounts' % len(accounts))
         self.response.out.write(jt.modelhelper.JsonQueryUtil.toArray('accounts',accounts))
 
 application = webapp.WSGIApplication([('/data/game/getAccountsByHighScore', GetAccountsByHighScore),
